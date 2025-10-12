6d3fa90d3896d495bdc7f15e9a7fe3abed100666
hosseinYasinzade <hosseinyasinzadeh2002@gmail.com>
2025-10-12 21:34:49 +0330

    feat: create base of project & icons components

---
+++ app/icons/icons.ts
@@ -0,0 +1,4 @@
+export { default as Notification } from "./notification";
+export { default as Search } from "./search";
+export { default as User } from "./user";
+export { default as Settings } from "./settings";
